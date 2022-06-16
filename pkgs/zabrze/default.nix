20976551c9c1b50cc66844a16691c3e6b27b9d6a
Shinnosuke Sawada <6warashi9@gmail.com>
2022-06-16 10:32:18 +0000

    feat: upgrade zabrze

--- pkgs/zabrze/default.nix
+++ pkgs/zabrze/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "zabrze";
-  version = "0.1.10";
+  version = "0.2.0";
 
   src = fetchFromGitHub {
     owner = "Ryooooooga";
     repo = pname;
     rev = "v${version}";
-    sha256 = "DgRFB/opfw9/z2l84YTwfeX7E+AwU4C435J5i14ZDcY=";
+    sha256 = "0jkyxyfsivwidbrjlvw3sbb9c2ibkldijx4dsh2wfqa30jwq49jl";
   };
 
-  cargoHash = "sha256-jGhtjg4sASp19KdiZerNoXtlInCsakzBidu0pbUHU7M=";
+  cargoHash = "sha256-obGzlA2bMQKCwoa8R9or6KEtlyaiB+FsLWR1ujmVbuQ=";
 
   meta = with lib; {
     description = "zsh completion utils";
