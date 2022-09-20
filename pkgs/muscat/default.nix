307e177ed906c92f2ae6ef200e720f65b3e2fdd4
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-20 13:36:08 +0900

    feat: upgrade muscat

--- pkgs/muscat/default.nix
+++ pkgs/muscat/default.nix
@@ -1,15 +1,18 @@
-{ lib, buildGo118Module, fetchFromGitHub }:
-
+{
+  lib,
+  buildGo118Module,
+  fetchFromGitHub,
+}:
 buildGo118Module rec {
   pname = "muscat";
-  version = "1.4.0";
+  version = "1.5.0";
   vendorSha256 = "0ICp5NHpKWHLUlk7Y/CqoP5x7LDJ9ExhPGdqdGrPBxA=";
 
   src = fetchFromGitHub {
     owner = "Warashi";
     repo = pname;
     rev = "v${version}";
-    sha256 = "09r2csrsal2w0kzc185b1mrzvavg7z0gcra5d4zav6qa8vmnqzrk";
+    sha256 = "sha256-bgoC4eM3ZE9chbMnmzwvAJE45FQyCDnS7nUgtr1SlvQ=";
   };
 
   postInstall = ''
