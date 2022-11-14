b51b1e0fcc82a39731d1c1169159663bc89ac04c
Shinnosuke Sawada <6warashi9@gmail.com>
2022-11-14 10:18:57 +0900

    feat: upgrade go

--- pkgs/muscat/default.nix
+++ pkgs/muscat/default.nix
@@ -1,9 +1,9 @@
 {
   lib,
-  buildGo118Module,
+  buildGo119Module,
   fetchFromGitHub,
 }:
-buildGo118Module rec {
+buildGo119Module rec {
   pname = "muscat";
   version = "1.5.0";
   vendorSha256 = "0ICp5NHpKWHLUlk7Y/CqoP5x7LDJ9ExhPGdqdGrPBxA=";
