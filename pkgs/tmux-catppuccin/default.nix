d325b52a0d6acb40427e0bd6913eee09f58b0b57
Shinnosuke Sawada <6warashi9@gmail.com>
2023-02-13 07:30:20 +0900

    feat: add tmuxplugins-catppuccin

---
+++ pkgs/tmux-catppuccin/default.nix
@@ -0,0 +1,23 @@
+{
+  lib,
+  tmuxPlugins,
+  fetchFromGitHub,
+}:
+tmuxPlugins.mkTmuxPlugin rec {
+  pluginName = "catppuccin";
+  version = "0.0.1";
+  rtpFilePath = "catppuccin.tmux";
+
+  src = fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "tmux";
+    rev = "e2561decc2a4e77a0f8b7c05caf8d4f2af9714b3";
+    sha256 = "sha256-6UmFGkUDoIe8k+FrzdzsKrDHHMNfkjAk0yyc+HV199M";
+  };
+
+  meta = with lib; {
+    homepage = "https://github.com/catppuccin/tmux";
+    description = "💽 Soothing pastel theme for Tmux!";
+    license = licenses.mit;
+  };
+}
